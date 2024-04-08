344d971d9fb42e79df2c81096952ec5c34e6bacf
wilmarp <wilmarp@uninorte.edu.co>
2024-04-08 12:06:56 -0500

    Versión 1

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'sql211.infinityfree.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'if0_36287200');//Usuario de tu base de datos
+define('DB_PASS', 'Ariadna0526');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'if0_36287200_losdeariadna');//Nombre de la base de datos
+?>
